fed194d0f6d37a800087fd58f50d0f0396b59ee1
Gitardo <gerardo.tejada.saracho@gmail.com>
2015-08-08 23:24:38 +0200

    pequeños arreglos

--- models/models.js
+++ models/models.js
@@ -35,3 +35,19 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){
+    if(count === 0) { // la tabla se inicializa sólo si está vacía
+      Quiz.create({ pregunta: 'Capital de Italia',
+                    respuesta: 'Roma'
+                 });
+      Quiz.create({ pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'
+                 })
+      .then(function(){console.log('Base de datos inicializada')});
+    };
+  });
+});
