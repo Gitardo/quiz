e745f43e2d0a0f8aae60c47c6d20386309de55a7
Gitardo <gerardo.tejada.saracho@gmail.com>
2015-08-16 19:46:10 +0200

    autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -22,9 +23,16 @@ router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
+// Definición de rutas de /comments
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
+// Definición de rutas de /session
+router.get('/login',  sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
+
+// Definición de rutas de Créditos
 router.get('/author', function(req, res) {
   res.render('author', { author: 'Gerardo Tejada Saracho' , errors: []});
 });
